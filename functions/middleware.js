f43b2225b44b1d20827b7ecde22bb26eb4cedecb
RPFO <admin@rpfo.ca>
2025-07-28 03:16:29 -0400

    Update middleware.js

--- functions/middleware.js
+++ functions/middleware.js
@@ -1,7 +1,8 @@
-// functions/middleware.js
+// /functions/middleware.js
 export async function onRequest(context) {
+  // Preflight
   if (context.request.method === 'OPTIONS') {
-    return new Response(null, { headers: corsHeaders(context.request) });
+    return new Response(null, { headers: cors(context.request) });
   }
 
   const res = await context.next();
@@ -9,17 +10,23 @@ export async function onRequest(context) {
 
   if (/^\/[a-z]{2}(-[A-Z]{2})?\.json$/.test(pathname)) {
     const h = new Headers(res.headers);
-    Object.entries(corsHeaders(context.request)).forEach(([k, v]) => h.set(k, v));
+    Object.entries(cors(context.request)).forEach(([k, v]) => h.set(k, v));
     return new Response(res.body, { status: res.status, headers: h });
   }
-
   return res;
 }
 
-function corsHeaders(req) {
+function cors(req) {
   const origin = req.headers.get('origin') || '';
+  const allow = [
+    'https://rpfo.webflow.io',
+    'https://www.rpfo.ca',
+    'https://go.outseta.com',
+    'https://app.outseta.com'
+  ];
+  const allowedOrigin = allow.includes(origin) ? origin : allow[0];
   return {
-    'Access-Control-Allow-Origin': origin,
+    'Access-Control-Allow-Origin': allowedOrigin,
     'Access-Control-Allow-Methods': 'GET, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     'Access-Control-Allow-Credentials': 'true',
